38f007a3ff0c65f651dd642b553714b87aa9c88a
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-03 13:53:20 +0800

    change config

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php 
 	session_start();
-	$koneksi = @new Mysqli("localhost", "root", "", "db_vajax");
+	$koneksi = @new Mysqli("localhost", "root", "", "db_infinity");
 
 	if (!$koneksi) {
 		echo "Connection Error";
