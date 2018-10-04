dce418ae1af5f67c948097067d612585b2b5752f
premaagus <premaagus@gmail.com>
2018-10-04 15:00:14 +0800

    validation javascript

--- admin/partials/addSiswa.php
+++ admin/partials/addSiswa.php
@@ -4,7 +4,7 @@
 	<form action="controller/add_siswaController.php" method="POST" onsubmit="return false"></form>
 	<div class="form-control form-panjang">
 		<h4>Nama Lengkap</h4>
-		<input type="text" name="" placeholder="Nama Lengkap">
+		<input type="text" name="nama_lengkap" placeholder="Nama Lengkap" id="nama_lengkap">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -12,7 +12,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Nama Depan</h4>
-		<input type="text" name="" placeholder="Nama Depan">
+		<input type="text" name="nama_depan" placeholder="Nama Depan" id="nama_depan">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -20,7 +20,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Nama Belakang</h4>
-		<input type="text" name="" placeholder="Nama Belakang">
+		<input type="text" name="nama_belakang" placeholder="Nama Belakang" id="nama_belakang">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -28,7 +28,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-panjang">
 		<h4>Email</h4>
-		<input type="email" name="" placeholder="Email">
+		<input type="email" name="email" placeholder="Email" id="email">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -36,7 +36,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-panjang">
 		<h4>Username</h4>
-		<input type="text" name="" placeholder="Username">
+		<input type="text" name="username" id="username" placeholder="Username">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -44,7 +44,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Password</h4>
-		<input type="text" name="" placeholder="Password">
+		<input type="text" name="password" id="password" placeholder="Password">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -52,7 +52,7 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Ulangi Password</h4>
-		<input type="text" name="" placeholder="Ulangi Password">
+		<input type="text" name="r_password" id="r_password" placeholder="Ulangi Password">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
@@ -60,19 +60,19 @@
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Tanggal Lahir</h4>
-		<input type="date" name="">
+		<input type="date" name="tanggal_lahir" id="tanggal_lahir">
 		<div class="alert-err">
 			<p>Nama Lengkap Tidak Boleh Kosong</p>
 			<div class="point-err"></div>
 		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
-		<h4>Grade</h4>
-		<select>
-			<option>Beginner</option>
-			<option>Intermediate</option>
-			<option>Expert</option>
-		</select>
+		<h4>Foto</h4>
+		<input type="file" name="tanggal_lahir" id="tanggal_lahir">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="add-new d-flex j-end i-ctr">
 		<div class="btn-add">
@@ -83,6 +83,14 @@
 
 <script>
 	var input = document.querySelectorAll('input');
+	var namaLengkap = document.getElementById('nama_lengkap');
+	var namaDepan = document.getElementById('nama_depan');
+	var namaBelakang = document.getElementById('nama_belakang');
+	var email = document.getElementById('email');
+	var	username = document.getElementById('username');
+	var password = document.getElementById('password');
+	var rPassword = document.getElementById('r_password');
+	var tanggalLahir = document.getElementById('tanggal_lahir');
 	for (var i = 0; i < input.length; i++){
 		input[i].addEventListener('keyup', function(){
 			if (this.value == "") {
