f51ccfebf0afdab35a213d64358a1aa38564309e
premaagus <premaagus@gmail.com>
2018-10-04 11:25:43 +0800

    alert validation in add siswa

--- admin/partials/addSiswa.php
+++ admin/partials/addSiswa.php
@@ -1,29 +1,70 @@
 <h1>Tambah Data</h1>
 <hr>
 <div class="container-addSiswa d-flex fd-row f-row j-ctr">
+	<form action="controller/add_siswaController.php" method="POST" onsubmit="return false"></form>
 	<div class="form-control form-panjang">
 		<h4>Nama Lengkap</h4>
-		<input type="text" name="" placeholder="Ketik nama lengkap kamu di sini">
+		<input type="text" name="" placeholder="Nama Lengkap">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
+	</div><!-- form-control  -->
+	<div class="form-control form-pendek">
+		<h4>Nama Depan</h4>
+		<input type="text" name="" placeholder="Nama Depan">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
+	</div><!-- form-control  -->
+	<div class="form-control form-pendek">
+		<h4>Nama Belakang</h4>
+		<input type="text" name="" placeholder="Nama Belakang">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-panjang">
 		<h4>Email</h4>
-		<input type="email" name="" placeholder="Ketik email kamu di sini">
+		<input type="email" name="" placeholder="Email">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-panjang">
 		<h4>Username</h4>
-		<input type="text" name="" placeholder="Ketik username kamu di sini">
+		<input type="text" name="" placeholder="Username">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Password</h4>
-		<input type="text" name="" placeholder="Ketik nama kamu di sini">
+		<input type="text" name="" placeholder="Password">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Ulangi Password</h4>
-		<input type="text" name="" placeholder="Ketik nama kamu di sini">
+		<input type="text" name="" placeholder="Ulangi Password">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Tanggal Lahir</h4>
 		<input type="date" name="">
+		<div class="alert-err">
+			<p>Nama Lengkap Tidak Boleh Kosong</p>
+			<div class="point-err"></div>
+		</div><!-- Alert-err -->
 	</div><!-- form-control  -->
 	<div class="form-control form-pendek">
 		<h4>Grade</h4>
@@ -35,7 +76,25 @@
 	</div><!-- form-control  -->
 	<div class="add-new d-flex j-end i-ctr">
 		<div class="btn-add">
-			<a href="#">Tambah Data</a>
+			<button onclick="addSiswa()">Tambah</button>
 		</div>
 	</div>
 </div><!-- container-addSiswa -->
+
+<script>
+	var input = document.querySelectorAll('input');
+	for (var i = 0; i < input.length; i++){
+		input[i].addEventListener('keyup', function(){
+			if (this.value == "") {
+				this.parentNode.querySelectorAll('.alert-err')[0].style.opacity = '1';
+			}
+			else{
+				this.parentNode.querySelectorAll('.alert-err')[0].style.opacity = '0';
+			}
+		});
+	}
+
+	function addSiswa(){
+
+	}
+</script>
