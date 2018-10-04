33d09c5f2c9df3b9c641ea20b4c75afa02e94fb8
premaagus <premaagus@gmail.com>
2018-10-04 17:02:31 +0800

    adding siswa

--- admin/partials/addSiswa.php
+++ admin/partials/addSiswa.php
@@ -1,85 +1,99 @@
 <h1>Tambah Data</h1>
 <hr>
-<div class="container-addSiswa d-flex fd-row f-row j-ctr">
-	<form action="controller/add_siswaController.php" method="POST" onsubmit="return false"></form>
-	<div class="form-control form-panjang">
-		<h4>Nama Lengkap</h4>
-		<input type="text" name="nama_lengkap" placeholder="Nama Lengkap" id="nama_lengkap">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Nama Depan</h4>
-		<input type="text" name="nama_depan" placeholder="Nama Depan" id="nama_depan">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Nama Belakang</h4>
-		<input type="text" name="nama_belakang" placeholder="Nama Belakang" id="nama_belakang">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-panjang">
-		<h4>Email</h4>
-		<input type="email" name="email" placeholder="Email" id="email">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-panjang">
-		<h4>Username</h4>
-		<input type="text" name="username" id="username" placeholder="Username">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Password</h4>
-		<input type="text" name="password" id="password" placeholder="Password">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Ulangi Password</h4>
-		<input type="text" name="r_password" id="r_password" placeholder="Ulangi Password">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Tanggal Lahir</h4>
-		<input type="date" name="tanggal_lahir" id="tanggal_lahir">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="form-control form-pendek">
-		<h4>Foto</h4>
-		<input type="file" name="tanggal_lahir" id="tanggal_lahir">
-		<div class="alert-err">
-			<p>Nama Lengkap Tidak Boleh Kosong</p>
-			<div class="point-err"></div>
-		</div><!-- Alert-err -->
-	</div><!-- form-control  -->
-	<div class="add-new d-flex j-end i-ctr">
-		<div class="btn-add">
-			<button onclick="addSiswa()">Tambah</button>
+<form action="" method="POST">
+	<div class="container1 d-flex f-row">
+
+		<div class="form-control-block">
+			<p>Nama Lengkap</p>
+			<input type="text" name="nama_lengkap" id="nama_lengkap" placeholder="Input Nama Lengkap Disini..." required>
+			<div class="alert-err">
+				<p>Nama Lengkap tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+
+		<div class="form-control">
+			<p>Nama Depan</p>
+			<input type="text" name="nama_depan" id="nama_depan" placeholder="Input Username Disini..." required>
+			<div class="alert-err">
+				<p>Nama Depan Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+		<div class="form-control">
+			<p>Nama Belakang</p>
+			<input type="text" name="nama_belakang" id="nama_belakang" placeholder="Input Password Disini..." required>
+			<div class="alert-err">
+				<p>Nama Belakang Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+
+		<div class="form-control-block">
+			<p>Email</p>
+			<input type="text" name="email" id="email" placeholder="Input Nama Lengkap Disini..." required>
+			<div class="alert-err">
+				<p>Email Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+
+		<div class="form-control-block">
+			<p>Username</p>
+			<input type="text" name="username" id="username" placeholder="Input Nama Lengkap Disini..." required>
+			<div class="alert-err">
+				<p>Username Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+
+		<div class="form-control">
+			<p>Password</p>
+			<input type="password" name="password" id="password" placeholder="Input No Telepon Disini..." required>
+			<div class="alert-err">
+				<p>Password Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+		<div class="form-control">
+			<p>Ulangi Password</p>
+			<input type="text" name="r_password" id="r_password" placeholder="Input Alamat Disini..." required>
+			<div class="alert-err">
+				<p>Ulangi Password Anda</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+
+		<div class="form-control-block">
+			<p>Tanggal Lahir</p>
+			<input type="date" name="tanggal_lahir" id="tanggal_lahir" placeholder="Input E-mail Disini..." required>
+			<div class="alert-err">
+				<p>Tanggal Lahir Tidak Boleh Kosong</p>
+				<div class="point-err"></div>
+			</div>
+		</div>
+		<div class="form-control-block">
+			<p>Foto Profil</p>
+			<input type="file" name="profile_img" id="profile_img" placeholder="Input E-mail Disini...">
+		</div>
+		<div class="form-control-block">
+			<p>Kelas</p>
+			<select name="id_kelas">
+				<option value="Admin">Admin</option>
+				<option value="Kasir">Kasir</option>
+			</select>
 		</div>
 	</div>
-</div><!-- container-addSiswa -->
+	<div class="btn-add">
+		<button type="submit" name="btn-submit">Submit</button>
+	</div>
+</form>
+
+<?php 
+	if (isset($_POST['btn-submit'])) {
+		echo "Clicked";
+	}
+ ?>
 
 <script>
 	var input = document.querySelectorAll('input');
@@ -91,6 +105,7 @@
 	var password = document.getElementById('password');
 	var rPassword = document.getElementById('r_password');
 	var tanggalLahir = document.getElementById('tanggal_lahir');
+	var profileImg = document.getElementById('profile_img');
 	for (var i = 0; i < input.length; i++){
 		input[i].addEventListener('keyup', function(){
 			if (this.value == "") {
@@ -100,9 +115,6 @@
 				this.parentNode.querySelectorAll('.alert-err')[0].style.opacity = '0';
 			}
 		});
-	}
-
-	function addSiswa(){
 
 	}
 </script>
