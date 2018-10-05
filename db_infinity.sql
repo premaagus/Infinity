b3e720d1341a7477bf18b4377111a9bc1ada4173
premaagus <premaagus@gmail.com>
2018-10-05 12:51:33 +0800

    update database

--- db_infinity.sql
+++ db_infinity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2018 at 05:28 AM
+-- Generation Time: Oct 05, 2018 at 06:51 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -19,308 +19,54 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_infinity`
+-- Database: `dbjualbukurpl3`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_guru`
+-- Table structure for table `tbuser`
 --
 
-CREATE TABLE `tb_guru` (
-  `id_guru` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_jadwal`
---
-
-CREATE TABLE `tb_jadwal` (
-  `id_jadwal` int(11) NOT NULL,
-  `id_kelas` int(11) NOT NULL,
-  `id_mapel` int(11) NOT NULL,
-  `jam_mulai` datetime NOT NULL,
-  `jam_selesai` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_kelas`
---
-
-CREATE TABLE `tb_kelas` (
-  `id_kelas` int(11) NOT NULL,
-  `nama_kelas` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_level`
---
-
-CREATE TABLE `tb_level` (
-  `id_level` int(11) NOT NULL,
-  `nama_level` varchar(50) NOT NULL
+CREATE TABLE `tbuser` (
+  `KodeUser` int(11) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `NamaLengkap` varchar(30) NOT NULL,
+  `Telp` varchar(15) NOT NULL,
+  `Alamat` varchar(50) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Level` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_level`
+-- Dumping data for table `tbuser`
 --
 
-INSERT INTO `tb_level` (`id_level`, `nama_level`) VALUES
-(1, 'Admin'),
-(2, 'Siswa'),
-(3, 'Guru');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_mapel`
---
-
-CREATE TABLE `tb_mapel` (
-  `id_mapel` int(11) NOT NULL,
-  `nama_mapel` varchar(30) NOT NULL,
-  `id_guru` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_materi`
---
-
-CREATE TABLE `tb_materi` (
-  `id_materi` int(11) NOT NULL,
-  `id_mapel` int(11) NOT NULL,
-  `nama_materi` varchar(30) NOT NULL,
-  `desc_materi` text NOT NULL,
-  `file_materi` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_pengumuman`
---
-
-CREATE TABLE `tb_pengumuman` (
-  `id_pengumuman` int(11) NOT NULL,
-  `capt_pengumuman` varchar(30) NOT NULL,
-  `desc_pengumuman` text NOT NULL,
-  `waktu_pengumuman` datetime NOT NULL,
-  `id_mapel` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_siswa`
---
-
-CREATE TABLE `tb_siswa` (
-  `id_siswa` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_kelas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_siswa`
---
-
-INSERT INTO `tb_siswa` (`id_siswa`, `id_user`, `id_kelas`) VALUES
-(1, 2, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_siswa_tugas`
---
-
-CREATE TABLE `tb_siswa_tugas` (
-  `id_siswa` int(11) NOT NULL,
-  `id_tugas` int(11) NOT NULL,
-  `nilai` int(4) NOT NULL,
-  `file_tugas` varchar(50) NOT NULL,
-  `tgl_pengumpulan` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_tugas`
---
-
-CREATE TABLE `tb_tugas` (
-  `id_tugas` int(11) NOT NULL,
-  `id_mapel` int(11) NOT NULL,
-  `id_kelas` int(11) NOT NULL,
-  `nama_tugas` varchar(30) NOT NULL,
-  `desc_tugas` text NOT NULL,
-  `tugas_mulai` datetime NOT NULL,
-  `tugas_selesai` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_user`
---
-
-CREATE TABLE `tb_user` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  `nama_lengkap` varchar(50) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `profile_img` varchar(50) NOT NULL,
-  `profile_name` varchar(50) NOT NULL,
-  `id_level` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_user`
---
-
-INSERT INTO `tb_user` (`id_user`, `username`, `email`, `password`, `nama_lengkap`, `tanggal_lahir`, `profile_img`, `profile_name`, `id_level`) VALUES
-(2, 'premaagusp', 'pramaagus@gmail.com', '$2y$12$EdOkPMTiwVfI7wMY.Bxrrujk8wSCC8FyA/YWuEquTBH9DekER31qW', 'premaagus', '2001-12-05', '', '', 2),
-(3, 'premaagus', 'premaagus@gmail.com', '$2y$12$o0c7OV4VFcyu.rEtyCedDuAI17DUxKAq1YnDx6Ey29FGd8fYerG5K', 'I Wayan Prema Agus Prasetya', '2001-08-30', '', '', 1),
-(4, 'bul', 'ryanardito25@gmail.com', '$2y$12$eJxnW98xKMZzMXubEgjZ0ebR.BQDO7wfOL8mGEQKZASmvx5YEw1O2', 'Ryan Ardito Zahwan Ragazzo', '2001-04-14', '', '', 1);
+INSERT INTO `tbuser` (`KodeUser`, `Username`, `Password`, `NamaLengkap`, `Telp`, `Alamat`, `Email`, `Level`) VALUES
+(2, 'ryanardito', 'kupretelek14', 'Ryan Ardito Zahwan Ragazzo', '081339760178', 'Jalan Gunung Himalaya 1B No.2', 'ryanardito25@gmail.com', 'Admin'),
+(3, 'admin', 'admin', 'admin pertama', '081123123123', 'Computer', 'admin@gmail.com', 'Admin'),
+(4, 'premaagusp', 'sanoer34', 'Prema Agus', '087761661669', 'asdkjalksdj', 'asasdlka@gmail.com', 'Kasir');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_guru`
+-- Indexes for table `tbuser`
 --
-ALTER TABLE `tb_guru`
-  ADD PRIMARY KEY (`id_guru`);
-
---
--- Indexes for table `tb_jadwal`
---
-ALTER TABLE `tb_jadwal`
-  ADD PRIMARY KEY (`id_jadwal`);
-
---
--- Indexes for table `tb_kelas`
---
-ALTER TABLE `tb_kelas`
-  ADD PRIMARY KEY (`id_kelas`);
-
---
--- Indexes for table `tb_level`
---
-ALTER TABLE `tb_level`
-  ADD PRIMARY KEY (`id_level`);
-
---
--- Indexes for table `tb_mapel`
---
-ALTER TABLE `tb_mapel`
-  ADD PRIMARY KEY (`id_mapel`);
-
---
--- Indexes for table `tb_materi`
---
-ALTER TABLE `tb_materi`
-  ADD PRIMARY KEY (`id_materi`);
-
---
--- Indexes for table `tb_pengumuman`
---
-ALTER TABLE `tb_pengumuman`
-  ADD PRIMARY KEY (`id_pengumuman`);
-
---
--- Indexes for table `tb_siswa`
---
-ALTER TABLE `tb_siswa`
-  ADD PRIMARY KEY (`id_siswa`);
-
---
--- Indexes for table `tb_tugas`
---
-ALTER TABLE `tb_tugas`
-  ADD PRIMARY KEY (`id_tugas`);
-
---
--- Indexes for table `tb_user`
---
-ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`id_user`);
+ALTER TABLE `tbuser`
+  ADD PRIMARY KEY (`KodeUser`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tb_guru`
+-- AUTO_INCREMENT for table `tbuser`
 --
-ALTER TABLE `tb_guru`
-  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_jadwal`
---
-ALTER TABLE `tb_jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_kelas`
---
-ALTER TABLE `tb_kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_level`
---
-ALTER TABLE `tb_level`
-  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tb_mapel`
---
-ALTER TABLE `tb_mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_materi`
---
-ALTER TABLE `tb_materi`
-  MODIFY `id_materi` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_pengumuman`
---
-ALTER TABLE `tb_pengumuman`
-  MODIFY `id_pengumuman` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_siswa`
---
-ALTER TABLE `tb_siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tb_tugas`
---
-ALTER TABLE `tb_tugas`
-  MODIFY `id_tugas` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_user`
---
-ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `tbuser`
+  MODIFY `KodeUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
