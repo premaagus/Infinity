2c5387a2e1784dc656a16a668f4e22af553657d0
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-06 15:14:08 +0800

    mapel

--- admin/partials/detailMapel.php
+++ admin/partials/detailMapel.php
@@ -1,6 +1,23 @@
 <h1>IPA</h1>
 <a href="#"><h2>Made Sutisna</h2></a>
 <hr>
-<div class="container-detailMapel">
-	
-</div><!-- container-detailMapek -->
+<div class="container-detailMapel d-flex fd-row f-row j-ctr">
+	<div class="card-detailMapel">
+		<a href="#">
+			<img src="../img/pattern/pattern1.png">
+			<h3>Materi</h3>
+		</a>
+	</div><!-- card-detailMapel -->
+	<div class="card-detailMapel">
+		<a href="#">
+			<img src="../img/pattern/pattern2.png">
+			<h3>Tugas</h3>
+		</a>
+	</div><!-- card-detailMapel -->
+	<div class="card-detailMapel">
+		<a href="#">
+			<img src="../img/pattern/pattern3.png">
+			<h3>Pengumuman</h3>
+		</a>
+	</div><!-- card-detailMapel -->
+</div><!-- container-detailMapel -->
