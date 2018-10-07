a53832ce3d77072d56216599cd87cf3e70643f48
premaagus <premaagus@gmail.com>
2018-10-08 01:39:41 +0800

    adding database

--- db_infinity (1).sql
+++ db_infinity (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 05, 2018 at 10:18 AM
+-- Generation Time: Oct 07, 2018 at 07:38 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -30,16 +30,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_guru` (
   `id_guru` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `nik` varchar(6) NOT NULL,
+  `jenis_kelamin` varchar(15) NOT NULL,
+  `tempat_lahir` varchar(25) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `bidang_ilmu` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_guru`
 --
 
-INSERT INTO `tb_guru` (`id_guru`, `id_user`) VALUES
-(1, 6),
-(2, 7);
+INSERT INTO `tb_guru` (`id_guru`, `id_user`, `nama_lengkap`, `nik`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `agama`, `telp`, `alamat`, `bidang_ilmu`) VALUES
+(4, 14, 'Luh Putu Ayu Desiani', '3444', 'Perempuan', 'Denpasar', '1985-05-30', 'Hindu', '081337123123', 'Denpasar', 'Database Information'),
+(5, 15, 'Evi Dwi Krisna', '3445', 'Perempuan', 'Denpasar', '1992-03-13', 'Hindu', '081122223334', 'Denpasar', 'Matematika'),
+(6, 16, 'Putu Dilia Dewi', '3446', 'Perempuan', 'Denpasar', '1988-04-30', 'Hindu', '08123111222', 'Denpasar', 'Bahasa Inggris');
 
 -- --------------------------------------------------------
 
@@ -63,16 +73,17 @@ CREATE TABLE `tb_jadwal` (
 
 CREATE TABLE `tb_kelas` (
   `id_kelas` int(11) NOT NULL,
-  `nama_kelas` varchar(30) NOT NULL
+  `nama_kelas` varchar(30) NOT NULL,
+  `ruangan` varchar(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_kelas`
 --
 
-INSERT INTO `tb_kelas` (`id_kelas`, `nama_kelas`) VALUES
-(1, 'XII RPL 3'),
-(2, 'XII RPL 4');
+INSERT INTO `tb_kelas` (`id_kelas`, `nama_kelas`, `ruangan`) VALUES
+(1, 'XII RPL 3', '9'),
+(2, 'XII RPL 4', '2');
 
 -- --------------------------------------------------------
 
@@ -103,7 +114,7 @@ INSERT INTO `tb_level` (`id_level`, `nama_level`) VALUES
 CREATE TABLE `tb_mapel` (
   `id_mapel` int(11) NOT NULL,
   `nama_mapel` varchar(30) NOT NULL,
-  `id_guru` int(11) NOT NULL,
+  `jumlah_jam` int(5) NOT NULL,
   `background_mapel` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -111,8 +122,11 @@ CREATE TABLE `tb_mapel` (
 -- Dumping data for table `tb_mapel`
 --
 
-INSERT INTO `tb_mapel` (`id_mapel`, `nama_mapel`, `id_guru`, `background_mapel`) VALUES
-(1, 'IPA', 1, 'merah.png');
+INSERT INTO `tb_mapel` (`id_mapel`, `nama_mapel`, `jumlah_jam`, `background_mapel`) VALUES
+(6, 'Basis Data', 4, 'merah.png'),
+(7, 'Administrasi Basis Data', 2, 'merah.png'),
+(8, 'Matematika', 4, 'orange.png'),
+(9, 'Bahasa Inggris', 2, 'biru.png');
 
 -- --------------------------------------------------------
 
@@ -151,6 +165,15 @@ CREATE TABLE `tb_pengumuman` (
 CREATE TABLE `tb_siswa` (
   `id_siswa` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `nis` varchar(6) NOT NULL,
+  `jenis_kelamin` varchar(15) NOT NULL,
+  `tempat_lahir` varchar(20) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `agama` varchar(20) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `jurusan` varchar(30) NOT NULL,
   `id_kelas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -158,8 +181,8 @@ CREATE TABLE `tb_siswa` (
 -- Dumping data for table `tb_siswa`
 --
 
-INSERT INTO `tb_siswa` (`id_siswa`, `id_user`, `id_kelas`) VALUES
-(3, 5, 1);
+INSERT INTO `tb_siswa` (`id_siswa`, `id_user`, `nama_lengkap`, `nis`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `agama`, `telp`, `alamat`, `jurusan`, `id_kelas`) VALUES
+(6, 13, 'Made Deva Mahayana', '3415', 'Laki - Laki', 'Denpasar', '2000-12-24', 'Hindu', '081123123123', 'Kesiman', 'Rekayasa Perangkat Lunak', 1);
 
 -- --------------------------------------------------------
 
@@ -202,8 +225,6 @@ CREATE TABLE `tb_user` (
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(150) NOT NULL,
-  `nama_lengkap` varchar(50) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
   `profile_img` varchar(50) NOT NULL,
   `profile_name` varchar(50) NOT NULL,
   `id_level` int(11) NOT NULL
@@ -213,12 +234,13 @@ CREATE TABLE `tb_user` (
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`id_user`, `username`, `email`, `password`, `nama_lengkap`, `tanggal_lahir`, `profile_img`, `profile_name`, `id_level`) VALUES
-(3, 'premaagus', 'premaagus@gmail.com', '$2y$12$o0c7OV4VFcyu.rEtyCedDuAI17DUxKAq1YnDx6Ey29FGd8fYerG5K', 'I Wayan Prema Agus Prasetya', '2001-08-30', '', '', 1),
-(4, 'bul', 'ryanardito25@gmail.com', '$2y$12$eJxnW98xKMZzMXubEgjZ0ebR.BQDO7wfOL8mGEQKZASmvx5YEw1O2', 'Ryan Ardito Zahwan Ragazzo', '2001-04-14', '', '', 1),
-(5, 'premaagusppp', 'premaaguspras@gmail.com', '$2y$12$.Dtn/Jc.Kg6L8IBUo8Z4x.Uzjth9SC2BeHXb3RRl6sr.n8maLK1dm', 'Prema Agus Prasetya', '2001-08-30', '5bb706c97c5a8.jpg', 'Prema Agus', 2),
-(6, 'razr', 'razr@gmail.com', '$2y$12$5fXsWjxaeHBZMbb6BQEUOub2Uch51V5tZmnCQJYd.F5EbR8WeNLGe', 'Ryan Ardito Zahwan Ragazzo', '2001-08-30', '5bb7070e511f1.jpg', 'Ryan Ardito', 3),
-(7, 'razzzrr', 'ryanardito255@gmail.com', '$2y$12$ABru6IiFBhojFAfJ2ZM4EecvEWYOtvoZvCrWV0bzyZnwW7WtegMUy', 'Wayan Ryan Ardito', '2001-08-30', '5bb70893caf16.jpg', 'Ryan Muji', 3);
+INSERT INTO `tb_user` (`id_user`, `username`, `email`, `password`, `profile_img`, `profile_name`, `id_level`) VALUES
+(10, 'premaagus', 'premaagus@gmail.com', '$2y$12$Ibvy5.rwholmc0TvUTqflOX2xXl1zbzEI8ZMa549hx3JRwmI7X9Hu', '5bba0fb1194e0.jpg', 'Prema Agus', 1),
+(11, 'bul', 'ryanardito25@gmail.com', '$2y$12$Uvhr7ZWAF38WsUf9GaPxC.Lz5Nm9e5MgeKh3qDFpJAe405JpCssba', '5bba17b85d110.jpg', 'Ryan Ardito', 1),
+(13, 'devamahayana', 'devamahayana@gmail.com', '$2y$12$vfbcSSontDJtOroAtfSySOyrNuOa.0h7JZpFX9Sh5K4fXSJf63SCy', '5bba200895392.jpg', 'Deva Mahyana', 2),
+(14, 'ayudesiani', 'ayudesiani@gmail.com', '$2y$12$GV5MzL6taCaKMk/c8Jxkvekn/ikT9wJaNrMnrR0slLygwN4AxvEau', '5bb6b3f607716.png', 'Ayu Desiani', 3),
+(15, 'evidwi', 'evidwi@gmail.com', '$2y$12$5FxqvOZpaANS3qwZulW4m.Zj/boWm1osnnn01YvJ44dMST8oLmUv6', '5bba4043c1f27.png', 'Evi Dwi', 3),
+(16, 'diliadewi', 'diliadewi@gmail.com', '$2y$12$UMr/vnFYvZldSlDAPYsw7uhJrNsj3989hXecLpUopWkTUE5hsx8F.', '5bba410e9d0e7.png', 'Dilia Dewi', 3);
 
 --
 -- Indexes for dumped tables
@@ -292,7 +314,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_guru`
 --
 ALTER TABLE `tb_guru`
-  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_jadwal`
@@ -316,7 +338,7 @@ ALTER TABLE `tb_level`
 -- AUTO_INCREMENT for table `tb_mapel`
 --
 ALTER TABLE `tb_mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tb_materi`
@@ -334,7 +356,7 @@ ALTER TABLE `tb_pengumuman`
 -- AUTO_INCREMENT for table `tb_siswa`
 --
 ALTER TABLE `tb_siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_tugas`
@@ -346,7 +368,7 @@ ALTER TABLE `tb_tugas`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
