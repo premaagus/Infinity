f2db4c42c3de8caa862b0ff6e3addc615054e7c4
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-09 23:39:43 +0800

    tugas

--- admin/partials/viewTugas.php
+++ admin/partials/viewTugas.php
@@ -1,1 +1,55 @@
-viewTugas.php
+<h1>Jadwal</h1>
+<hr>
+
+<div class="add-new d-flex j-end i-ctr">
+	<div class="btn-add">
+		<a href="index.php?menu=jadwal&action=add">Tambah Jadwal</a>
+	</div><!-- btn-add -->
+</div><!-- add-new -->
+<div class="table-control">
+	<table>
+		<tr>
+			<th>No</th>
+			<th>Nama Tugas</th>
+			<th>Deskripsi Tugas</th>
+			<th>Tugas Mulai</th>
+			<th>Tugas Selesai</th>
+		</tr>
+		<?php 
+
+			$queryJadwal = $koneksi->query("SELECT * FROM tb_jadwal");
+			$no = 1;
+			while ($dataJadwal = $queryJadwal->fetch_assoc()) {
+				$jam_mulai = $dataJadwal['jam_mulai'];
+				$jam_selesai = $dataJadwal['jam_selesai'];
+
+				//key
+				$id_mapel 	= $dataJadwal['id_mapel'];
+				$id_guru 	= $dataJadwal['id_guru'];
+				$id_kelas 	= $dataJadwal['id_kelas'];
+
+				//query
+				$queryMapel 	= $koneksi->query("SELECT * FROM tb_mapel WHERE id_mapel = $id_mapel");
+				$queryGuru 		= $koneksi->query("SELECT * FROM tb_guru AS a INNER JOIN tb_user AS b ON a.id_user = b.id_user WHERE id_guru = $id_guru");
+				$queryKelas 	= $koneksi->query("SELECT * FROM tb_kelas WHERE id_kelas = $id_kelas");
+
+				//data
+				$dataMapel = $queryMapel->fetch_assoc();
+				$dataGuru = $queryGuru->fetch_assoc();
+				$dataKelas = $queryKelas->fetch_assoc();
+
+				?>
+				<tr>
+					<td><?php echo $no ?></td>
+					<td><?php echo $dataMapel['nama_mapel'] ?></td>
+					<td><?php echo $dataGuru['profile_name'] ?></td>
+					<td><?php echo $dataJadwal['hari'] ?></td>
+					<td><?php echo date("H:i", strtotime("$jam_mulai")); ?></td>
+				</tr>
+				<?php
+				$no++;
+			}
+
+		 ?>
+	</table>
+</div>
