b1deb9fad470411f06fd37f268ea0fe9e996f7b5
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-11 16:48:13 +0800

    dashboard

--- admin/partials/dashboard.php
+++ admin/partials/dashboard.php
@@ -1,1 +1,36 @@
-dashboard.php
+<h1>Master Data</h1>
+<hr>
+<div class="container-dashboard d-flex f-row fd-row j-ard">
+	<div class="dashboard-show d-flex">
+		<div class="dashboard-nilai">
+			<h1>107</h1>
+		</div><!-- dashboard-nilai -->
+		<div class="dashboard-text">
+			<h1>Total Siswa</h1>
+		</div><!-- dashboard-nilai -->
+	</div><!-- dashboard-kelas -->
+	<div class="dashboard-show d-flex">
+		<div class="dashboard-nilai">
+			<h1>46</h1>
+		</div><!-- dashboard-nilai -->
+		<div class="dashboard-text">
+			<h1>Total Guru</h1>
+		</div><!-- dashboard-nilai -->
+	</div><!-- dashboard-kelas -->
+	<div class="dashboard-show d-flex">
+		<div class="dashboard-nilai">
+			<h1>12</h1>
+		</div><!-- dashboard-nilai -->
+		<div class="dashboard-text">
+			<h1>Total Pelajaran</h1>
+		</div><!-- dashboard-nilai -->
+	</div><!-- dashboard-kelas -->
+	<div class="dashboard-show d-flex">
+		<div class="dashboard-nilai">
+			<h1>10</h1>
+		</div><!-- dashboard-nilai -->
+		<div class="dashboard-text">
+			<h1>Total Kelas</h1>
+		</div><!-- dashboard-nilai -->
+	</div><!-- dashboard-kelas -->
+</div><!-- container-dashboard -->
