40797480a08b644b8e6dfa2e84fb45528bf4e72f
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-12 21:29:05 +0800

    responsive login

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<link rel="icon" href="img/logo/icon.png">
 	<link rel="stylesheet" type="text/css" href="css/fontawesome.css">
 	<link rel="stylesheet" href="css/alert.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
