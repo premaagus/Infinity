aec116edff55de6a8b08e7b0eb26d8acc2355cae
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-14 19:45:09 +0800

    homepage

---
+++ homepage.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AcademyLab - Beranda</title>
+		<link rel="icon" href="img/logo/icon.png">
+		<link rel="stylesheet" href="css/homepage.css">
+		<link rel="stylesheet" href="css/fontawesome.css">
+		<link rel="stylesheet" href="css/alert.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="navbar">
+				<div class="max-width d-flex f-row fd-row j-btw">
+					<img src="img/logo-branding-purple.png">
+					<ul>
+						<li><a href="#">Beranda</a></li>
+						<li><a href="#">Fitur</a></li>
+						<li><a href="#">Petunjuk</a></li>
+						<li><a href="#">Kontak</a></li>
+						<li><a class="login" href="#">Masuk</a></li>
+					</ul>
+				</div><!-- max-width -->
+			</div><!-- navbar -->
+
+			<div class="hero d-flex">
+				<div class="max-width d-flex i-ctr">
+					<div>
+						<h1>Belajar lebih mudah, kapanpun dan <br> di manapun.</h1>
+						<a href="">Mulai</a>
+					</div>
+					<div class="hero-img">
+						<img src="img/hero.png">
+					</div>
+				</div><!-- max-width -->
+			</div><!-- hero -->
+
+			<div class="container-question d-flex">
+				<div class="img d-flex i-ctr">
+					<img src="img/faq.png">
+				</div><!-- img -->
+				<div class="text d-flex i-ctr">
+					<div>
+						<h1>Apa itu Academy Lab ?</h1>
+						<p>Academy Lab adalah suatu website E-Learing di mana seluruh kegiatan belajar mengajar dilakukan secara online. Kapan saja dan di mana saja oleh guru maupun siswa.</p>
+					</div>
+				</div><!-- text -->
+			</div><!-- container-question -->
+
+			<div class="container-fitur d-flex i-ctr">
+				<div class="max-width">
+					<h1>Fitur dari AcademyLab</h1>
+					<div class="box-fitur d-flex j-ard">
+						<div class="fitur">
+							<div class="fitur-img-materi">
+								<img src="img/materi.png">
+							</div><!-- fitur-img -->
+							<h3>Materi</h3>
+							<p>Guru dapat memberi materi kepada siswa, dan siswa dapat mengunduh materi tersebut untuk dipelajari.</p>
+						</div><!-- fitur -->
+						<div class="fitur">
+							<div class="fitur-img">
+								<img src="img/tugas.png">
+							</div><!-- fitur-img -->
+							<h3>Tugas</h3>
+							<p>Setelah memberikan materi, guru juga dapat memberikan tugas kepada siswa. Kemudian, siswa dapat mengumpulkan tugas tersebut dalam bentuk berkas PDF atau  Microsoft Word.</p>
+						</div><!-- fitur -->
+						<div class="fitur">
+							<div class="fitur-img">
+								<img src="img/announc.png">
+							</div><!-- fitur-img -->
+							<h3>Pengumuman</h3>
+							<p>Guru dapat memberikan pengumuman berupa pergantian jam pelajaran, tugas yang direvisi, dan nilai yang diperoleh siswa.</p>
+						</div><!-- fitur -->
+					</div><!-- box-fitur -->
+				</div><!-- max-width -->
+			</div><!-- container-fitur -->
+
+			<div class="container-contact d-flex i-ctr">
+				<div class="max-width">
+					<h1>Hubungi Kami</h1>
+					<p>Ingin mengajukan pertanyaan, saran, dan lain sebagainya ? Silahkan hubungi kami melalui kontak yang kami berikan atau isi form di bawah ini.</p>
+					<div class="form d-flex f-row j-ard">
+						<div class="form-pendek">
+							<h2>NAMA</h2>
+							<input type="text" name="" placeholder="Ketik nama kamu di sini">
+						</div>
+						<div class="form-pendek">
+							<h2>E-Mail</h2>
+							<input type="email" name="" placeholder="Ketik e-mail kamu di sini">
+						</div>
+						<div class="form-panjang">
+							<h2>DESKRIPSI</h2>
+							<textarea placeholder="Ketik apa yang ingin kamu sampaikan untuk Academy Lab di sini"></textarea>
+						</div><!-- panjang -->
+						<a href="">KIRIM</a>
+					</div><!-- form -->
+					<div class="kontak d-flex j-btw fd-row f-row">
+						<div class="kotak d-flex j-ctr i-ctr">
+							<i class="fab fa-facebook-f"></i>
+							| SMK TI Bali Global Denpasar
+						</div><!-- kotak -->
+						<div class="kotak d-flex j-ctr i-ctr">
+							<i class="fab fa-instagram"></i>
+							| globalitidps
+						</div><!-- kotak -->
+						<div class="kotak d-flex j-ctr i-ctr">
+							<i class="fab fa-twitter"></i>
+							| GlobaliTIDPS
+						</div><!-- kotak -->
+						<div class="kotak d-flex j-ctr i-ctr">
+							<i class="fas fa-envelope-open"></i>
+							| admin@smkti-baliglobal.sch.id
+						</div><!-- kotak -->
+					</div><!-- kontak -->
+				</div><!-- max-width -->
+			</div><!-- container-contact -->
+
+			<div class="footer d-flex i-ctr">
+				<div class="max-width">
+					<h1>Berlangganan bersama kami</h1>
+					<p>Ketik e-mail kamu di bawah dan berlangganan bersama kami. Kamu akan mendapat e-mail notifikasi jika website dalam perbaikan maupun jika ada penambahan fitur.</p>
+					<div class="subs d-flex j-ctr">
+						<input type="email" placeholder="Ketik e-mail kamu di sini">
+						<a href="#" class="kirim">KIRIM</a>
+					</div><!-- subs -->
+					<div class="copyright d-flex f-row fd-row j-btw">
+						<div>
+							<h4>&copy; 2018 AcademyLab</h4>
+						</div>
+						<div>
+							<h4>Made With <i class="fas fa-heart"></i></h4>
+						</div>
+					</div><!-- copyright -->
+				</div><!-- max-width -->
+			</div><!-- footer -->
+
+		</div><!-- wrapper -->
+	</body>
+</html>
