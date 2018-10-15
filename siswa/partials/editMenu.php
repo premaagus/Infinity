8f69543976b68d42550d0b61ddf65e5f6bc75d52
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-16 01:06:14 +0800

    button edit profile + password

--- siswa/partials/editMenu.php
+++ siswa/partials/editMenu.php
@@ -1,1 +1,12 @@
-editMenu.php
+<h1>Profile</h1>
+<hr>
+<div class="container-detailSiswa">
+	<div class="detailBackground"></div><!-- detailBackground -->
+	<div class="detailFoto">
+		<img src="../img/profile/5bba0fb1194e0.jpg">
+	</div><!-- detailFoto -->
+	<div class="button-detail d-flex j-ctr">
+		<a href="#" class="edit-profil">Ubah Profil</a>
+		<a href="#" class="edit-password">Ubah Password</a>
+	</div><!-- button-detail -->
+</div><!-- container-detailSiswa -->
