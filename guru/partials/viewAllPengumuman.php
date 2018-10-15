2110aa3546dd515b61a847c1a0251b37ae78593d
Ragazzo <36918361+ryanardito@users.noreply.github.com>
2018-10-15 19:10:20 +0800

    Update guru/partials/viewAllPengumuman.php

--- guru/partials/viewAllPengumuman.php
+++ guru/partials/viewAllPengumuman.php
@@ -1,1 +1,25 @@
-viewAllPengumuman.php
+<?php 
+	$id_kelas = $_GET['id_kelas'];
+	$id_mapel = $_GET['id_mapel'];
+ ?>
+
+<h1>Pengumuman</h1>
+<hr>
+
+<div class="container-pengumuman">
+	<div class="pengumuman d-flex">
+		<div class="img-pengumuman">
+			<img src="images/profile/profile1.jpg">
+		</div><!-- img-pengumuman -->
+		<div class="text-pengumuman">
+			<div class="d-flex j-btw">
+				<h4>Evi Dwi</h4>
+				<h5>15 Oktober 2018</h5>
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			<h6>14.60</h6>
+			<div class="acc"></div>
+		</div><!-- text-pengumuman -->
+	</div><!-- pengumuman -->
+</div><!-- container-pengumuman -->
